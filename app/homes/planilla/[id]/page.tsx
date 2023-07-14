6bb20c480fd97060a7f9b703f4c61c085ef7e412
Seba <seba.avergara@gmail.com>
2023-07-13 21:26:15 -0400

    cambios menores

--- app/homes/planilla/[id]/page.tsx
+++ app/homes/planilla/[id]/page.tsx
@@ -1,88 +1,95 @@
-// import axios from "axios";
+import ShowPlanillas from "@/components/ShowPlanillas";
+import axios from "axios";
 
-// const getCarrera = async (id: string) => {
-//   const res = await axios.get("http://localhost:3000/api/carrera/" + id);
-//   return res.data;
-// };
+const getCarrera = async (id: string) => {
+  const res = await axios.get("http://localhost:3000/api/carrera/" + id);
+  return res.data;
+};
 
-"use client";
+const Page = async ({ params: { id } }: { params: { id: string } }) => {
+  const carrera = await getCarrera(id);
 
-import ShowBitacora from "@/components/ShowBitacora";
-import ShowHistorico from "@/components/ShowHistorico";
-import ShowStatus from "@/components/ShowStatus";
-import Image from "next/image";
-import React from "react";
+  return <ShowPlanillas carrera={carrera} />;
+};
 
-interface Car {
-  id: number;
-  name: string;
-}
+export default Page;
 
-export default function Home() {
-  const [selectedCar, setSelectedCar] = React.useState<Car | null>(null);
+// import ShowBitacora from "@/components/ShowBitacora";
+// import ShowHistorico from "@/components/ShowHistorico";
+// import ShowStatus from "@/components/ShowStatus";
+// import Image from "next/image";
+// import React from "react";
 
-  const cars: Car[] = [
-    {
-      id: 1,
-      name: "Bitacora",
-    },
-    {
-      id: 2,
-      name: "Status",
-    },
-    {
-      id: 3,
-      name: "Historico",
-    },
-  ];
+// interface Car {
+//   id: number;
+//   name: string;
+// }
 
-  return (
-    <main className="flex flex-col items-center justify-center h-screen w-screen p-24">
-      <h1 className="text-4xl font-bold">Selecciona una opción para visualizar las planillas: </h1>
-      <div className="">
-        <ul className="flex flex-wrap">
-          {cars.map((car) => (
-            <li
-              key={car.id}
-              className={`flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md
-                  ${selectedCar?.id === car.id ? "bg-green-500" : ""}
-                `}
-              onClick={() => setSelectedCar(car)}
-            >
-              <span>{car.name}</span>
-              <Image
-                src="/images/arrow-right.svg"
-                alt="arrow right"
-                width={16}
-                height={16}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex flex-col items-center justify-center p-24">
-        {selectedCar?.name === "Bitacora" && (
-          <div className="flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md">
-            <div className="w-full">
-              <ShowBitacora />
-            </div>
-          </div>
-        )}
-        {selectedCar?.name === "Status" && (
-          <div className="flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md">
-            <div className="w-full">
-              <ShowStatus />
-            </div>
-          </div>
-        )}
-        {selectedCar?.name === "Historico" && (
-          <div className="flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md">
-            <div className="w-full">
-              <ShowHistorico />
-            </div>
-          </div>
-        )}
-      </div>
-    </main>
-  );
-}
+// export default function Home() {
+//   const [selectedCar, setSelectedCar] = React.useState<Car | null>(null);
+
+//   const cars: Car[] = [
+//     {
+//       id: 1,
+//       name: "Bitacora",
+//     },
+//     {
+//       id: 2,
+//       name: "Status",
+//     },
+//     {
+//       id: 3,
+//       name: "Historico",
+//     },
+//   ];
+
+//   return (
+//     <main className="flex flex-col items-center justify-center h-screen w-screen p-24">
+//       <h1 className="text-4xl font-bold">Selecciona una opción para visualizar las planillas: </h1>
+//       <div className="">
+//         <ul className="flex flex-wrap">
+//           {cars.map((car) => (
+//             <li
+//               key={car.id}
+//               className={`flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md
+//                   ${selectedCar?.id === car.id ? "bg-green-500" : ""}
+//                 `}
+//               onClick={() => setSelectedCar(car)}
+//             >
+//               <span>{car.name}</span>
+//               <Image
+//                 src="/images/arrow-right.svg"
+//                 alt="arrow right"
+//                 width={16}
+//                 height={16}
+//               />
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//       <div className="flex flex-col items-center justify-center p-24">
+//         {selectedCar?.name === "Bitacora" && (
+//           <div className="flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md">
+//             <div className="w-full">
+//               <ShowBitacora />
+//             </div>
+//           </div>
+//         )}
+//         {selectedCar?.name === "Status" && (
+//           <div className="flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md">
+//             <div className="w-full">
+//               <ShowStatus />
+//             </div>
+//           </div>
+//         )}
+//         {selectedCar?.name === "Historico" && (
+//           <div className="flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md">
+//             <div className="w-full">
+//               <ShowHistorico />
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </main>
+//   );
+// }
---
+++ components/ShowPlanillas.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import ShowBitacora from "@/components/ShowBitacora";
+import ShowHistorico from "@/components/ShowHistorico";
+import ShowStatus from "@/components/ShowStatus";
+import { Carrera } from "@/interfaces";
+import Image from "next/image";
+import React from "react";
+
+interface Planilla {
+  id: number;
+  name: string;
+}
+
+interface Props {
+  carrera: Carrera;
+}
+
+const Home: React.FC<Props> = ({ carrera }) => {
+  const [selectedPlanilla, setSelectedPlanilla] =
+    React.useState<Planilla | null>(null);
+
+  const planillas: Planilla[] = [
+    {
+      id: 1,
+      name: "Bitacora",
+    },
+    {
+      id: 2,
+      name: "Status",
+    },
+    {
+      id: 3,
+      name: "Historico",
+    },
+  ];
+
+  return (
+    <main className="flex flex-col items-center justify-center h-screen w-screen p-24">
+      <h1 className="text-4xl font-bold">
+        Selecciona una opción para visualizar las planillas:{" "}
+      </h1>
+      <div className="">
+        <ul className="flex flex-wrap">
+          {planillas.map((planilla) => (
+            <li
+              key={planilla.id}
+              className={`flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md
+                  ${selectedPlanilla?.id === planilla.id ? "bg-green-500" : ""}
+                `}
+              onClick={() => setSelectedPlanilla(planilla)}
+            >
+              <span>{planilla.name}</span>
+              <Image
+                src="/images/arrow-right.svg"
+                alt="arrow right"
+                width={16}
+                height={16}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-col items-center justify-center p-24">
+        {selectedPlanilla?.name === "Bitacora" && (
+          <div className="flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md">
+            <div className="w-full">
+              <ShowBitacora />
+            </div>
+          </div>
+        )}
+        {selectedPlanilla?.name === "Status" && (
+          <div className="flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md">
+            <div className="w-full">
+              <ShowStatus />
+            </div>
+          </div>
+        )}
+        {selectedPlanilla?.name === "Historico" && (
+          <div className="flex items-center justify-center p-8 my-4 bg-gray-100 rounded-md">
+            <div className="w-full">
+              <ShowHistorico />
+            </div>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
