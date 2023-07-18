21522e588b6243d35922bddbc9b933220c7f5832
ScarletteAA <93626663+ScarletteAA@users.noreply.github.com>
2023-07-17 22:06:05 -0400

    correcion error

---
+++ prisma/migrations/20230718015246_correccion_errores/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `is_Carrera_Espejo` on the `carrera` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "carrera" DROP COLUMN "is_Carrera_Espejo";
